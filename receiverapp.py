7d8b090708d2b0b032d33a32debbf4051f74a477
Jonah <jonah_lefkoff@hotmail.com>
2019-12-05 12:02:35 -0700

    added correct broker

--- receiverapp.py
+++ receiverapp.py
@@ -4,10 +4,12 @@ import subprocess
 import time
 
 class makeProcess:
-    def make(self, arg1, arg2):
-        self.internalProcess = subprocess.Popen([arg1,arg2])
-    def kill(self):
-        self.internalProcess.kill()
+    def makeMe(self, arg1, arg2):
+        self.internalProcess = subprocess.Popen(['sudo',arg1,arg2])
+        print("Process spawned with PID: %s" % self.internalProcess.pid)
+        self.pgid = os.getpgid(internalProcess.pid)
+    def killMe(self):
+        self.internalProcess.terminate()
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
@@ -20,10 +22,11 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     if(str(msg.payload) == "b'p1'"):
         # os.system("python3 hworld.py")
-        mainProcess.make('python3','hworld.py')
+        mainProcess.makeMe('python3','hworld.py')
         time.sleep(5)
     elif(str(msg.payload) == "b'qp1'"):
-        mainProcess.kill()
+        print("kill received")
+        mainProcess.killMe()
     
 
 client = mqtt.Client()
@@ -32,7 +35,7 @@ client.on_message = on_message
 
 mainProcess = makeProcess()
 
-client.connect("jlmbp.local", 1883, 60)
+client.connect("mqttbroker", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
