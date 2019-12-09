ec7811750c7928a45fb7769bf1bf51d8d627c358
Jonah <jonah_lefkoff@hotmail.com>
2019-12-09 15:09:48 -0700

    added new program paths

--- receiverapp.py
+++ receiverapp.py
@@ -10,6 +10,7 @@ class makeProcess:
         self.pgid = os.getpgid(internalProcess.pid)
     def killMe(self):
         self.internalProcess.terminate()
+        # subprocess.run('kill -s ' + self.internalProcess.pid)
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
@@ -20,20 +21,52 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    # if(str(msg.payload == "b'startup'")):
+    #     backProcess.makeMe('python','/home/pi/git_repos/fish_tank/space_tank.py')
+    # elif(str(msg.payload == "b'close_startup'")):
+    #     backProcess.killMe()
     if(str(msg.payload) == "b'p1'"):
         # os.system("python3 hworld.py")
-        mainProcess.makeMe('python3','hworld.py')
-        time.sleep(5)
+        mainProcess.makeMe('python','/home/pi/glenn/mqtt_gamepad/wormgame_mqtt.py')
+        #time.sleep(5)
     elif(str(msg.payload) == "b'qp1'"):
-        print("kill received")
-        mainProcess.killMe()
+        print("kill received p1")
+        client.publish("shutdown")
+    elif(str(msg.payload) == "b'p2'"):
+        mainprocess2.makeMe('python','/home/pi/glenn/audio_mqtt/dual_display.py')
+    elif(str(msg.payload) == "b'p2b'"):
+        mainProcess.makeMe('python','/home/pi/glenn/audio_mqtt/mic_full.py')
+    elif(str(msg.payload) == "b'qp2'"):
+        print("kill received p2")
+        client.publish("shutdown")
+    elif(str(msg.payload) == "b'p3'"):
+        mainProcess.makeMe('python','/home/pi/glenn/temp/DonutsMQTT.py')
+    elif(str(msg.payload) == "b'qp3'"):
+        print("kill received p3")
+        client.publish("shutdown")
+    elif(str(msg.payload) == "b'p4'"):
+        mainProcess.makeMe('python','/home/pi/glenn/mqtt_gamepad/cycles_4p.py')
+    elif(str(msg.payload) == "b'qp4'"):
+        print("kill received p4")
+        client.publish("shutdown")
+    elif(str(msg.payload) == "b'p5'"):
+        mainProcess.makeMe('python','/home/pi/glenn/mqtt_camera/camera_mqtt.py')
+    elif(str(msg.payload) == "b'p5b'"):
+        mainProcess.makeMe('python','/home/pi/glenn/mqtt_camera/display_mqtt.py')
+    elif(str(msg.payload) == "b'qp5'"):
+        print("kill received p5")
+        client.publish("shutdown")
+
     
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
+backProcess = makeProcess()
 mainProcess = makeProcess()
+mainprocess2 = makeProcess()
 
 client.connect("mqttbroker", 1883, 60)
 
